f4ae8f1a538ec37822810ff908be29be5235b12f
Neeraj Parmar <neerajparmar748@gmail.com>
2026-02-23 23:30:27 +0530

    Scrapping by providing web URL

---
+++ Project_Part_1.py
@@ -0,0 +1,40 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+
+def fetch_page(url):
+    response = requests.get(url)
+    return response.text
+
+
+def parsing(html):
+    soup = BeautifulSoup(html, "html.parser")
+
+    title = soup.title.string if soup.title else "Title not found"
+
+    body_content = soup.get_text(separator=" ")
+
+    all_links = []
+    for tag in soup.find_all("a", href=True):
+        all_links.append(tag["href"])
+
+    return title.strip(), body_content.strip(), all_links
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print("Link is not provided. Please provide one URL as an argument.")
+        exit()
+
+    url = sys.argv[1]
+    html = fetch_page(url)
+
+    title, body, links = parsing(html)
+
+    print("\npage title:\n", title)
+    print("\npage body:\n", body[:])
+    print("\nLinks:")
+    for l in links:
+        print(l)
